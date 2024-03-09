7663b64ce31f8a596e2404093d2da7afa1b5bd67
Asanta Mariam Mathew <asantamariammathew@MBA.local>
2024-03-09 06:30:13 +0530

    Crypto

---
+++ index.js
@@ -0,0 +1,65 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req,res) => {
+    res.json('hii')
+})
+
+app.get('/convert', (req,res) => {
+    const toCurrency = req.query.to_currency
+    const fromCurrency = req.query.from_currency
+
+    console.log('toCurrency', toCurrency)
+    console.log('fromCurrency', fromCurrency)
+    const options = {
+        method: 'GET',
+        url: 'https://alpha-vantage.p.rapidapi.com/query',
+        params: {
+            from_currency: fromCurrency,
+            function: 'CURRENCY_EXCHANGE_RATE',
+            to_currency: toCurrency
+        },
+        headers: {
+            'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+            'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com'
+        }
+    }
+
+    axios.request(options).then((response) => {
+        res.json(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'])  
+    }).catch((error) => {
+        console.error(error)
+    })
+})
+
+app.get('/news', (req,res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://crypto-news16.p.rapidapi.com/news/top/5',
+        // https://h-crypto-news.p.rapidapi.com/cryptonews
+        headers: {
+          'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+          'X-RapidAPI-Host': 'crypto-news16.p.rapidapi.com'
+        //   h-crypto-news.p.rapidapi.com
+        }
+       
+      }
+  
+      axios.request(options).then((response) => {
+        res.json(response.data)
+   
+      }).catch((error) => {
+        console.error(error)
+  
+      })
+})
+
+
+app.listen(8000, () => console.log(`Server is running on port ${PORT}`))
